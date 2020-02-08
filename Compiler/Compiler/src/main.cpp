8dcf5c1885bf3f32c6901bb2399b5f9dc227d698
Braineeee <braineeee@users.noreply.github.com>
2020-02-08 15:45:34 -0700

    Got a scanner class implemented! Need to fill it out!

--- Compiler/Compiler/src/main.cpp
+++ Compiler/Compiler/src/main.cpp
@@ -1,5 +1,14 @@
 #include "../antlr4-runtime/antlr4-runtime.h"
+#include "string"
+#include <Compiler\Compiler\src\scanner.cpp>
 
-int main() {
+using namespace antlr4;
 
+int main(int argc, char** argv) {
+	if(argc == 2)
+	{ 
+		std::string fileName = argv[1];
+		Compilers::Scanner *scanning = new Compilers::Scanner(new ANTLRFileStream(fileName));
+	}
+	return 0;
 }
---
+++ Compiler/Compiler/src/scanner.cpp
@@ -0,0 +1,19 @@
+#include "string"
+namespace Compilers
+{
+	class Scanner /*Write our scanner code within this class!*/
+	{
+		Scanner()
+		{
+
+
+		}
+
+
+		Scanner(std::string fileName)
+		{
+
+		}
+	};
+};
+	
