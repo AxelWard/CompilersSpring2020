7269b8c4b4816cd24bedd11b607563dd2617845b
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-02-25 18:33:38 -0700

    Finish Code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import LITTLE.LITTLELexer;
 import LITTLE.LITTLEParser;
 import org.antlr.v4.runtime.*;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 // Code for Axel Ward, Ahmed Naji, and Jacob Ziehli
@@ -12,20 +13,25 @@ public class Main {
         String fileParts[] = source.split("/");
         String fileName = fileParts[(fileParts.length)-1].replace(".micro", "");
 
-        Parser p = new Parser();
-
         try {
             CharStream cs = CharStreams.fromFileName(source);
             LITTLE.LITTLELexer lexer = new LITTLELexer(cs);
             LITTLE.LITTLEParser parser = new LITTLEParser(new CommonTokenStream(lexer));
 
-            Vocabulary v = parser.getVocabulary();
+            parser.program();
+            int syntaxErrors = parser.getNumberOfSyntaxErrors();
 
-            if(args.length == 2) {
+            if(args.length >= 2) {
                 fileName = args[1] + "/" + fileName;
             }
 
-
+            FileWriter f = new FileWriter(fileName + ".out");
+            if(syntaxErrors == 0) {
+                f.append("Accepted");
+            } else {
+                f.append("Not accepted");
+            }
+            f.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
